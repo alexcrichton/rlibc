a2ad6736e870de7114fd1c6afffe3da6f567e620
Travis CI User <travis@example.org>
2018-09-21 18:53:58 +0000

    Documentation for alexcrichton/rlibc@defb486

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var N = null;var searchIndex = {};
+searchIndex["rlibc"]={"doc":"A bare-metal library supplying functions rustc may lower code to","items":[[5,"memcpy","rlibc","",N,N],[5,"memmove","","",N,N],[5,"memset","","",N,N],[5,"memcmp","","",N,N]],"paths":[]};
+initSearch(searchIndex);
